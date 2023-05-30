312f8f8a77ea2a964fc96b0608ecb04dc1d6e485
scutiller <108549026+scutiller@users.noreply.github.com>
2023-05-30 02:42:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,6 @@ st.header=("Hola Mundo!!!")
 st.title("Bienvenidos a la pagina que no hace nada aun. Scutiller. Agregado")
 
 st.code("print ('Hola')")
+
+st.code("for i in range(20):
+        print("Hola")")
